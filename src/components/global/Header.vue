0f3dc45948c767b8a7666b3d149691c9d79297a6
riya09 <faria.ayir@gmail.com>
2022-05-28 14:12:00 +0600

    iadd: header component with styling

---
+++ src/components/global/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="header">
+    <h1>Make Your Tea</h1>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+#header {
+  position: sticky;
+  top: 0;
+  padding: 5px 10px;
+  text-align: center;
+  background: hsla(219, 40%, 55%, 0.8);
+  h1 {
+    color: #fff;
+    margin: 0;
+    font-size: 32px;
+    font-family: 'Lobster', cursive;
+  }
+}
+</style>
