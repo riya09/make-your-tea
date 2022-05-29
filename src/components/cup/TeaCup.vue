7309fc27d3ff732f3b5237e7fe7c15f986537307
riya09 <faria.ayir@gmail.com>
2022-05-29 16:20:00 +0600

    add: fill logic and styling for flavors

--- src/components/cup/TeaCup.vue
+++ src/components/cup/TeaCup.vue
@@ -7,7 +7,15 @@
         width="350"
         height="372"
         clip-path="url(#clip-cup)"
-        :fill="cupFill"
+        fill="url(#stroke-color)"
+      />
+      <rect
+        ref="rect-flavor"
+        x="0"
+        width="350"
+        height="372"
+        clip-path="url(#clip-cup)"
+        fill="url(#tea-flavor)"
       />
       <path
         d="M0 58H47M344 58H294M294 58L267 370H76L47 58M294 58H47"
@@ -17,8 +25,27 @@
       <!-- dynamic stroke gradient of tea cup -->
       <defs>
         <linearGradient id="stroke-color" x1="172" y1="0" x2="172" y2="344">
-          <stop :stop-color="cupStroke.stop1" />
-          <stop offset="1" :stop-color="cupStroke.stop2" />
+          <stop :stop-color="cupFill.stop1" />
+          <stop offset="1" :stop-color="cupFill.stop2" />
+        </linearGradient>
+      </defs>
+      <!---->
+      <!--dynamic gradient of tea flavor-->
+      <defs>
+        <linearGradient
+          id="tea-flavor"
+          x1="195"
+          y1="0"
+          x2="195"
+          y2="273"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop :stop-color="flavorColor.stop1" stop-opacity="0" />
+          <stop
+            offset="1"
+            :stop-color="flavorColor.stop2"
+            stop-opacity="0.56"
+          />
         </linearGradient>
       </defs>
       <!---->
@@ -42,32 +69,31 @@ export default {
       type: String,
       default: "",
     },
+    flavor: {
+      type: String,
+      default: "",
+    },
   },
   data() {
     return {
-      strokeColor: {
+      fillColor: {
         green: { stop1: "#8ECC9F", stop2: "#00B78B" },
         black: { stop1: "#8C8C8C", stop2: "#081C17" },
         milk: { stop1: "#F5BF33", stop2: "#E3CA44" },
+        orange: { stop1: "#FFD978", stop2: "#FFA928" },
+        strawberry: { stop1: "#F8B1B1", stop2: "#F97B7B" },
+        lemon: { stop1: "#ECFF78", stop2: "#FFF850" },
+        kiwi: { stop1: "#9CFF8C", stop2: "#50FF96" },
+        blueberry: { stop1: "#BDE7FF", stop2: "#82FFF7" },
       },
     };
   },
   computed: {
-    cupStroke() {
-      const teaType = this.tea;
-      return {
-        stop1: this.strokeColor[teaType]
-          ? this.strokeColor[teaType].stop1
-          : "#8FCCE7",
-        stop2: this.strokeColor[teaType]
-          ? this.strokeColor[teaType].stop2
-          : "#8FCCE7",
-      };
-    },
     cupFill() {
-      return this.tea && this.strokeColor[this.tea]
-        ? this.strokeColor[this.tea].stop1
-        : "none";
+      return this.getGradientColor(this.tea);
+    },
+    flavorColor() {
+      return this.getGradientColor(this.flavor);
     },
   },
   methods: {
@@ -79,11 +105,26 @@ export default {
         }, 100);
       }
     },
+    getGradientColor(type) {
+      return {
+        stop1: this.fillColor[type]
+          ? this.fillColor[type].stop1
+          : "#8FCCE7",
+        stop2: this.fillColor[type]
+          ? this.fillColor[type].stop2
+          : "#8FCCE7",
+      };
+    },
   },
   watch: {
     tea(val, newVal) {
       if (val !== newVal) {
         this.toggleDomClass("rect", "fill");
+      }
+    },
+    flavor(val, newVal) {
+      if (val !== newVal) {
+        this.toggleDomClass("rect-flavor", "fill");
       }
     },
   },
@@ -97,7 +138,7 @@ svg {
   rect {
     y: 372;
     &.fill {
-      y: 100;
+      y: 80;
       transition: 0.4s ease-out;
     }
   }
