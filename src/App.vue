5a0a76e7af846c886babb6eb5922790f6821f588
riya09 <faria.ayir@gmail.com>
2022-06-10 11:46:00 +0600

    style: import fonts from google api

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,11 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Lobster&family=Open+Sans&display=swap");
+* {
+  margin: 0;
+  padding: 0;
+  font-family: sans-serif;
+}
 </style>
