f74a4972695e816718b057c3cb2d0540cb2b66ed
riya09 <faria.ayir@gmail.com>
2022-06-10 12:19:00 +0600

    chore: remove unncessary semi-colon

--- src/App.vue
+++ src/App.vue
@@ -7,13 +7,13 @@
 </template>
 
 <script>
-import Header from "@/components/global/Header.vue";
+import Header from "@/components/global/Header.vue"
 
 export default {
   components: {
     Header,
   },
-};
+}
 </script>
 
 <style lang="scss">
