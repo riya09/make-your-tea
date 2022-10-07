bb0d51f3ec46328e8e805209fcf48fe0abf29aa8
riya09 <faria.ayir@gmail.com>
2022-10-07 18:51:03 +0600

    style: declared colors globally

--- src/App.vue
+++ src/App.vue
@@ -21,10 +21,21 @@ export default {
 
 <style lang="scss">
 @import url("https://fonts.googleapis.com/css2?family=Lobster&family=Open+Sans&display=swap");
+:root {
+  --ripe-mango:hsl(46, 98%, 56%);
+  --cornflower:hsl(195, 66%, 76%);
+  --frost:hsl(207, 41%, 58%);
+  --lotion:hsl(45, 100%, 98%);
+  --white: hsl(0, 0%, 100%);
+  --naples-yellow:hsl(46, 98%, 68%);
+}
 * {
   margin: 0;
   padding: 0;
   font-family: sans-serif;
+}
+body {
+  background-color: var(--lotion);
 }
 .container {
   display: grid;
